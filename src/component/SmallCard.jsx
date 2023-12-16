ca345497c5a0643fc1e38269f0c8ca1a30e09d2c
muktiupa <muktiupa@gmail.com>
2023-12-16 14:22:10 +0530

    update new changes

--- src/component/SmallCard.jsx
+++ src/component/SmallCard.jsx
@@ -1,14 +1,16 @@
 import { HiOutlineComputerDesktop } from "react-icons/hi2";
 import allData from "../DataArry";
+import '../component/AdditionalCss.css'
 
 
 export default function SmallCard(props) {
     const {icon,title,bg}=props
     return(
         <>
-        <div className={`h-[150px] w-[150px] p-3 bg-[${bg}] rounded-[41px] mx-3`}>
+        <div className={`h-[230px] w-[230px] p-6 bg-[${bg}] smallcard-bg  m-2 rounded-[20px]`}>
         <span className="mt-">{icon}</span>
-        <h5 className="text-slate-100 font-semibold">{title}</h5>
+        <h2 className="text-slate-100 font-bold text-3xl pt-2">{title}</h2>
+        <hr className="px-[10px] mt-[20px]" />
 
         </div>
         
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -8,13 +8,13 @@ export default function homepage() {
         <>
 
 
-            <div className="w-full h-[90vh] flex items-center">
+            <div className="w-full h-[80vh] flex items-center">
                 <div className="w-1/2 font-bold " >
                     <h1 className="text-6xl text-[#FFA800]">Mastering Computers </h1>
                     <h3 className="my-5 text-3xl w-2/3">Transforming Students <br />for <span className="text-[#FFA800]">Future Success.</span></h3>
                     <p className="font-light text-sm w-2/3">Craft your success journey with <span className="font-bold">Vintage IT Academy's</span>  flexible career courses—online, offline, and hybrid</p>
                     {/* ---> */}
-                    <div className="mt-8 mb-6 w-1/2 flex items-center justify-around">
+                    <div className="mt-8 mb-4 w-full flex items-center justify-between">
 
                         <Button Btn="Contact" />
                         <div className=" ">
@@ -32,12 +32,18 @@ export default function homepage() {
 
             </div>
             <div className=" h-[50vh]" >
-                <h2 className="w-1/2 text-5xl font-semibold ">Browse Top Essential Career Courses</h2>
-                <div className="py-3 flex w-1/2 ">
+                <h2 className="w-1/2 text-4xl font-semibold ">Browse Top Essential Career Courses</h2>
+                <div className=" flex justify-between items-center w-full mt-5 box-border flex-wrap ">
                 {allData.SmallCardData.map((item) =>(
           <SmallCard icon={item.icon} title={item.title} bg={item.bg}/>
           ))}
-                        
+                       <svg width="155" height="155" viewBox="0 0 155 155" fill="none" xmlns="http://www.w3.org/2000/svg">
+<circle cx="77.5" cy="77.5" r="77.5" fill="#FAD9F8"/>
+<circle cx="77.5" cy="77.5" r="65.5" fill="#F9A7F3"/>
+<ellipse cx="77.5" cy="76" rx="49.5" ry="50" fill="#EE64E4"/>
+<path d="M84.3182 93.642L80.2841 89.6364L88.6932 81.2273H55V75.3182H88.6932L80.2841 66.9091L84.3182 62.9034L99.6875 78.2727L84.3182 93.642Z" fill="white"/>
+</svg>
+
                 </div>
 
             </div>
